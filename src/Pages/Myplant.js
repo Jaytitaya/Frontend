29c00dad15440ec6656ca3f8c84aa98f3b0f2ef7
Jaytitaya <6130142121@student.chula.ac.th>
2022-03-15 18:44:36 +0700

    publish

--- src/Pages/Myplant.js
+++ src/Pages/Myplant.js
@@ -1,4 +1,4 @@
-import React,{useState,useContext,useEffect} from 'react';
+import React,{Component,useState,useContext,useEffect} from 'react';
 import Navbar from '../Components/Navbar';
 import {Grid, Paper} from '@material-ui/core';
 import {Avatar} from '@material-ui/core';
@@ -8,25 +8,61 @@ import {useNavigate} from "react-router-dom";
 import Button from '@mui/material/Button';
 import Axios from 'axios';
 import { LoginContext } from '../App';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import FormControl from '@mui/material/FormControl';
+
+
 
 const Myplant=()=>{
     const avatarStyle={backgroundColor:'green', width:40, height:56, cursor: 'pointer'}
     const navigate = useNavigate();
     //const {username} = localStorage.username;
-    const {username} = useContext(LoginContext);
-    const [plantsList, setPlantsList] = useState([])
+    //const {username} = useContext(LoginContext);
+    const [plantsList, setPlantsList] = useState([]);
+    const plantnames = [];
+    const [pname, setPname] = useState([]);
+    const [names, setNames] = useState("");
+    const handleChange = (event) => {
+        setNames(event.target.value);
+      };
+
+    //useEffect(()=>{
+        //Axios.get('http://localhost:3001/plants', { withCredentials: true }).then((response)=>{
+        //    setPname(response.result);
+        //    console.log(pname)
+            
+            //for (let index = 0; index < pname.length; index++) {
+            //        plantnames.push(pname[index].plantname)
+            //}
+            //let unique = [...new Set(plantnames)]
+            
+        //});
+        //console.log(plantnames)
+    //},[]); 
+
     const getPlants = () =>{
         Axios.get('http://localhost:3001/plants', { withCredentials: true }).then((response)=>{
             setPlantsList(response.data);
         });
     }
-    
+
     return(
         <Grid align='center'>
             <Navbar/>
             <h2 className="app-front" style={{color:'#008000'}}>My Plant</h2>
-           
-            <Grid item xs={12}><Button onClick={getPlants} variant="contained" color="success" size="large" sx={{ mt: 3, mb: 2 }} style={{minWidth: '300px' }}>Show information</Button></Grid>
+            <Grid container spacing={3} direction="row" justifyContent="center" alignItems="center">
+            <Grid item xs={3} md={2} >
+                <FormControl sx={{ minWidth: 120 }}><InputLabel id="demo-simple-select-label" >Plant</InputLabel>
+                    <Select style={{minWidth: '200px'}} labelId="demo-multiple-name-label" id="demo-multiple-name" value={names}  label="Plant" input={<OutlinedInput label="Plant" />} onChange={handleChange}>
+                        {plantnames.map((plantnames) => (<MenuItem key={plantnames} value={plantnames}>{plantnames}</MenuItem>))}
+                    </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={3} md={2}><Button onClick={getPlants} variant="contained" color="success" size="large" sx={{ mt: 3, mb: 2 }} style={{minWidth: '210px' }}>Show information</Button></Grid>
+            </Grid>
             {plantsList.map((val,key)=>{
             return(
                  <div className='card-container'>
@@ -53,5 +89,6 @@ const Myplant=()=>{
         })}
         </Grid> 
     )
+
 }
 export default Myplant
