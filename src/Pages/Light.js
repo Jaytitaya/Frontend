75da63d34d4aec25f46f161968b3c0953012d260
Jaytitaya <6130142121@student.chula.ac.th>
2022-03-18 09:35:09 +0700

    publish

--- src/Pages/Light.js
+++ src/Pages/Light.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Navbar from '../Components/Navbar';
-import {Grid, TextField, Paper, Typography} from '@material-ui/core'
+import {Grid, TextField, Paper, Typography} from '@material-ui/core';
+import Button from '@mui/material/Button';
+import Axios from 'axios';
 
 function Light(){
     const paperStyle={padding:20,height:'90vh',width:700,margin:"10px auto",backgroundColor: '#f5f5f5'}
+    const [plantname,setPlantname]=useState("")
     return(
         
         <Grid align='center'>
@@ -16,6 +19,10 @@ function Light(){
                 <Grid item xs={3} >
                     <h2 className="app-front" style={{color:'#008000'}}>Light</h2>
                 </Grid>
+            </Grid>
+            <Grid container spacing={3} direction="row" justifyContent="center" alignItems="center" >
+                <Grid item xs={3} md={3}><TextField id="outlined-basic" label="Plant name" variant="outlined" onChange={(e) => setPlantname(e.target.value)}/></Grid>
+                <Grid item xs={3} md={2}><Button  variant="contained" color="success" size="large" sx={{ mt: 3, mb: 2 }} style={{minWidth: '210px' }}>Show information</Button></Grid>
             </Grid>
             <Grid container spacing={2} justifyContent="center">
                 <c>State :</c>
