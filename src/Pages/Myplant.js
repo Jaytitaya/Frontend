914db1187b151aa9bd9257b54bbe9d7c16245b71
Jaytitaya <6130142121@student.chula.ac.th>
2022-03-21 15:13:14 +0700

    publish

--- src/Pages/Myplant.js
+++ src/Pages/Myplant.js
@@ -1,13 +1,11 @@
 import React,{Component,useState,useContext,useEffect} from 'react';
 import Navbar from '../Components/Navbar';
 import {Grid, Paper} from '@material-ui/core';
-import {Avatar} from '@material-ui/core';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import {useNavigate} from "react-router-dom";
 import Button from '@mui/material/Button';
 import Axios from 'axios';
-import { LoginContext } from '../App';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import InputLabel from '@mui/material/InputLabel';
@@ -16,7 +14,17 @@ import FormControl from '@mui/material/FormControl';
 import IconButton from '@mui/material/IconButton';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import { grey } from '@mui/material/colors';
-import SvgIcon from '@mui/material/SvgIcon';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import { height } from '@mui/system';
+//import {Avatar} from '@material-ui/core';
+//import { LoginContext } from '../App';
+//import SvgIcon from '@mui/material/SvgIcon';
 
 const Myplant=()=>{
     const avatarStyle={backgroundColor:'green', width:40, height:56}
@@ -36,7 +44,7 @@ const Myplant=()=>{
       ];
     const handleChange = (event) => {
         setStage(event.target.value);
-      };
+    };
 
     function Icon(props){
         const selectstage = props;
@@ -44,6 +52,13 @@ const Myplant=()=>{
             return <CheckBoxIcon sx={{ color: grey[50] }}/>;
         }
     }
+    const [open, setOpen] = React.useState(false);
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
     //useEffect(()=>{
         //Axios.get('http://localhost:3001/plants', { withCredentials: true }).then((response)=>{
         //    setPname(response.result);
@@ -106,10 +121,28 @@ const Myplant=()=>{
                             <p>Humidity:{val.lowerhumid}-{val.higherhumid}%</p>
                             <p>pH:{val.lowerpH}-{val.higherpH}</p>
                         </div>
+                        <Dialog PaperProps={{ sx: { width: "100%", height: "70%" } }} open={open} onClose={handleClose}>
+                            <DialogTitle>Edit Information</DialogTitle>
+                            <DialogContent PaperProps={{  }} >
+                            
+                                <p><TextField style ={{width: '40%'}} required id="outlined-required" label="Plant name" defaultValue={val.plantname}/></p> 
+                                <p>Stage:<TextField style ={{width: '50%' ,height:'30%'}} required id="outlined-required" label="Stage" defaultValue={val.stage}/></p>
+                                <p>open time - close time:<TextField style ={{width: '20%'}} required id="outlined-required" label="open time" defaultValue={val.opentime}/>-<TextField style ={{width: '20%'}} required id="outlined-required" label="close time" defaultValue={val.closetime}/></p>
+                                <p>Temperature: <TextField style ={{width: '20%'}} required id="outlined-required" label="Temperature" defaultValue={val.lowertemp}/>-<TextField style ={{width: '20%'}} required id="outlined-required" label="Temperature" defaultValue={val.highertemp}/>Celsius</p>
+                                <p>Humidity:<TextField style ={{width: '20%'}} required id="outlined-required" label="Humidity" defaultValue={val.lowerhumid}/>-<TextField style ={{width: '20%'}} required id="outlined-required" label="Humidity" defaultValue={val.higherhumid}/> %</p>
+                                <p>pH:<TextField style ={{width: '20%'}} required id="outlined-required" label="pH" defaultValue={val.lowerpH}/>-<TextField style ={{width: '20%'}} required id="outlined-required" label="pH" defaultValue={val.higherpH}/></p>
+                                <FormControlLabel value="on" control={<Checkbox />} label="The plant is in this stage" labelPlacement="The plant is in this stage" />
+                            
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={handleClose}>Cancel</Button>
+                                <Button onClick={handleClose}>Save</Button>
+                            </DialogActions>
+                        </Dialog>
                     </div>
                     
                     <div className="btn">
-                        <IconButton><EditIcon sx={{ color: grey[50] }}/></IconButton>
+                        <IconButton onClick={handleClickOpen}><EditIcon sx={{ color: grey[50] }}/></IconButton>
                         <IconButton onClick={()=>deleteData(val.id)}><DeleteIcon sx={{ color: grey[50] }}/></IconButton>
                     </div>
                 </div>
