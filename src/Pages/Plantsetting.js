f833324c1123367f1d25e3766852f4308394804c
Jaytitaya <6130142121@student.chula.ac.th>
2022-03-29 22:44:35 +0700

    publish

--- src/Pages/Plantsetting.js
+++ src/Pages/Plantsetting.js
@@ -1,33 +1,64 @@
-import React, {useState,useEffect} from 'react'
+import * as React from 'react';
 import Navbar from '../Components/Navbar';
-import {Grid, Paper, Avatar, TextField, Typography, Link} from '@material-ui/core'
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import {useNavigate} from "react-router-dom";
-import Axios from 'axios';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const  Plantsetting=()=>{
-    const paperStyle={padding:20,height:'70vh',width:700,margin:"10px auto",backgroundColor: '#f5f5f5'}
-    const papergreen={padding:20,height:'50vh',width:400,margin:"10px auto",backgroundColor: '#8FBC8F'}
-    const navigate = useNavigate();
-    const [plantname,setPlantname]=useState("")
-    
-    
-    return(
-        <Grid align='center'>
-           <Navbar/>
-           <Paper elevation={0} style={paperStyle}>
-                <h2 className="app-front" style={{color:'#008000'}}>Plant</h2>
-                
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} ><TextField id="outlined-basic" label="Plant name" variant="outlined" onChange={(e) => setPlantname(e.target.value)}/></Grid>
-                        <Grid item xs={12} ><TextField id="outlined-basic" label="Life cycle" variant="outlined"/></Grid>
-                        <Grid item xs={12} ><TextField id="outlined-basic" label="Max height" variant="outlined"/></Grid>
-                        <Grid item xs={12}><Button variant="contained" color="success" size="large" sx={{ mt: 3, mb: 2 }} style={{minWidth: '225px' }}>Save</Button></Grid>
-                    </Grid> 
-                
-           </Paper>
+
+const theme = createTheme();
+
+export default function Plantsetting() {
+  
+
+  return (
+    <ThemeProvider theme={theme}>
+        <Navbar/>
+      <Grid container component="main" sx={{ height: '89vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={5}
+          sx={{
+            backgroundImage: 'url(/plant.jpg)',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={7} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <h2 className="app-front" style={{color:'#008000'}}>Plant</h2>
+            <Grid container spacing={2} align='center'>
+            <Grid item xs={12} ><TextField id="outlined-basic" label="Plant name" variant="outlined"/></Grid>
+            <Grid item xs={12} ><TextField id="outlined-basic" label="Utilization" variant="outlined"/></Grid>
+            <Grid item xs={12} ><TextField id="outlined-basic" label="Part used" variant="outlined"/></Grid>
+            <Grid item xs={12} ><TextField id="outlined-basic" label="Flowering season" variant="outlined"/></Grid>
+            <Grid item xs={12}><Button variant="contained" color="success" size="large" sx={{ mt: 3, mb: 2 }} style={{minWidth: '225px' }}>Save</Button></Grid>
+            </Grid>
+          </Box>
         </Grid>
-    )
+      </Grid>
+    </ThemeProvider>
+  );
 }
-
-export default Plantsetting
