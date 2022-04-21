ebaa250d00efc4ae0eebac8ee850aede064f0477
Jaytitaya <6130142121@student.chula.ac.th>
2022-04-21 19:56:14 +0700

    publish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import React, { useState,createContext } from 'react';
+
 import './App.css';
 import Login from './Pages/Login';
 import Signup from './Pages/Signup';
 import Addnewplant from './Pages/Addnewplant';
 import Myplant from './Pages/Myplant';
+import Card from './Components/Card';
 import Home from './Pages/Home';
 import Light from './Pages/Light';
 import Temp from './Pages/Temp';
@@ -16,33 +18,30 @@ import Plantsetting from './Pages/Plantsetting';
 import Farmsetting from './Pages/Farmsetting';
 import Plantform from './Pages/Plantform';
 import Farminfo from './Pages/Farminfo';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-const LoginContext = React.createContext();
+import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom';
+
+//const LoginContext = React.createContext();
+
 
 function App() {
-  
-  const token = localStorage.getItem("accessToken");
-  const [username, setUserName]=useState("");
-  
-  //if(!token){
-  //  return <Login/>
-  //}
+  //const [username, setUserName]=useState("");
 
   return (
-    <LoginContext.Provider value={{username, setUserName}}>
+    //<LoginContext.Provider value={{username, setUserName}}>
      <div className="wrapper" style={{backgroundColor: '#f5f5f5', height:'100%',width:'100%' }}>
      <BrowserRouter>
       <Routes>
-        
-        <Route exact path="/" element={<Login/>}/>
+        <Route exact path="/login" element={<Login/>}/>
         <Route exact path="/signup" element={<Signup/>}/>
         <Route exact path="/addnewplant" element={<Addnewplant/>}/>
         <Route exact path="/home" element={<Home/>}/>
+        <Route exact path="/" element={<Login/>}/>
         <Route exact path="/light" element={<Light/>}/>
         <Route exact path="/temp" element={<Temp/>}/>
         <Route exact path="/humid" element={<Humid/>}/>
         <Route exact path="/pH" element={<PH/>}/>
         <Route exact path="/myplant" element={<Myplant/>}/>
+        <Route exact path="/card" element={<Card/>}/>
         <Route exact path="/menuset" element={<Menuset/>}/>
         <Route exact path="/menuinfo" element={<Menuinfo/>}/>
         <Route exact path="/plantsetting" element={<Plantsetting/>}/>
@@ -52,8 +51,8 @@ function App() {
       </Routes>
      </BrowserRouter> 
      </div>
-    </LoginContext.Provider>
+  //  </LoginContext.Provider>
   );
 }
-export {LoginContext};
+//export {LoginContext};
 export default App;
